1defe8b2571a9439ff5d665ff6cd06aafc61d731
dtdtdz <69425431+dtdtdz@users.noreply.github.com>
2023-08-21 22:02:52 +0900

    feat: BOJ_7569_차우열-2주차 문제 해결 코드

---
+++ 3주차 (23. 08. 14. ~ 23. 08. 20.)/BOJ_7569_토마토/차우열.java
@@ -0,0 +1,86 @@
+package algo0821;
+
+/*
+1.summary:
+행N, 열M, 높이H를 입력받고, N*M*H배열에 토마토 정보를 입력받는다.
+매 날짜마다 익은 토마토는 인접한 토마토를 익은 토마토로 만들 때, 모든 토마토가 익은 토마토가 되기 위한 시간을 출력한다.
+만약 모든 토마토가 익을 수 없다면 -1을 출력한다.
+
+2.strategy:
+토마토의 좌표를 큐에 넣고 bfs를 사용한다.
+
+3.note:
+토마토에 인접한 토마토는 행,열,높이 중 하나의 값만 1차이나는 위치에 있는 토마토이다.
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_7569 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		
+		int[][][] tomato = new int[N][M][H];
+		
+		Queue<int[]> q = new ArrayDeque<int[]>();
+		
+		for (int k=0; k<H; k++) {
+			for (int i=0; i<N; i++) {
+				st = new  StringTokenizer(br.readLine());
+				for (int j=0; j<M; j++) {
+					tomato[i][j][k] = Integer.parseInt(st.nextToken());
+					if (tomato[i][j][k]==1) q.add(new int[] {i, j, k});
+				}
+			}
+		}
+		
+		int[][] direc = {{1,0,0},{0,1,0},{0,0,1},{-1,0,0},{0,-1,0},{0,0,-1}};
+		int cnt = -1;
+		while (!q.isEmpty()) {
+			cnt++;
+			Queue<int[]> newq = new ArrayDeque<int[]>();
+			while (!q.isEmpty()) {
+				int[] cur = q.poll();
+				for (int i=0; i<direc.length; i++) {
+					int dx = cur[0]+direc[i][0];
+					int dy = cur[1]+direc[i][1];
+					int dz = cur[2]+direc[i][2];
+					if (dx>=0 && dx<N && dy>=0 && dy<M && dz>=0 && dz<H && tomato[dx][dy][dz]==0) {
+						tomato[dx][dy][dz] = 1;
+						newq.add(new int[] {dx, dy, dz});
+					}
+				}
+			}
+			q = newq;
+		}
+		boolean flag = true;
+		for (int k=0; k<H; k++) {
+			for (int i=0; i<N; i++) {
+				for (int j=0; j<M; j++) {
+					if (tomato[i][j][k]==0) {
+						flag = false;
+						break;
+					}
+					if (flag) continue;
+					break;
+				}
+			}
+		}
+		if (flag) {
+			System.out.println(cnt);
+		} else {
+			System.out.println(-1);
+		}
+	}
+
+}
