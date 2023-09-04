fc5081c0c1e5275cb1fdb131403d52c5164e7f4d
nidolight <nidolight@outlook.com>
2023-09-04 22:04:27 +0900

    Update 허준호.java

--- 5주차 (23. 08. 28. ~ 23. 09. 03.)/BOJ_4485_녹색 옷 입은 애가 젤다지/허준호.java
+++ 5주차 (23. 08. 28. ~ 23. 09. 03.)/BOJ_4485_녹색 옷 입은 애가 젤다지/허준호.java
@@ -1,92 +1,71 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
+
+class Node implements Comparable<Node> {
+    int x, y, cost;
+
+    public Node(int x, int y, int cost) {
+        this.x = x;
+        this.y = y;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        return Integer.compare(this.cost, other.cost);
+    }
+}
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[][] board = new int[9][9];
-
-        for (int i = 0; i < 9; i++) {
-            String[] row = br.readLine().split(" ");
-            for (int j = 0; j < 9; j++) {
-                board[i][j] = Integer.parseInt(row[j]);
+        int tc = 1;
+        while (true) {
+            int n = Integer.parseInt(br.readLine());//동굴의 크기
+            if (n == 0) {
+                break;
             }
-        }
 
-        solveSudoku(board);//재귀 dfs
-        bw.write(boardToString(board));//결과 출력
-        bw.close();
-    }
-
-    public static boolean solveSudoku(int[][] board) {
-        int[] emptyCell = findEmptyCell(board);//빈칸 찾기
-
-        if (emptyCell == null) {//빈칸이 없는 경우 종료
-            return true;
-        }
-
-        int row = emptyCell[0];
-        int col = emptyCell[1];
-
-        for (int num = 1; num <= 9; num++) {
-            if (isValid(board, row, col, num)) {//문제조건에 맞는지 확인
-                board[row][col] = num;
-
-                if (solveSudoku(board)) {
-                    return true;
-                }
-
-                board[row][col] = 0;//빈칸 처리
-            }
-        }
-
-        return false;
-    }
-
-    public static int[] findEmptyCell(int[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == 0) {
-                    return new int[]{i, j};
+            int[][] cave = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    cave[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-        }
-        return null;
-    }
 
-    public static boolean isValid(int[][] board, int row, int col, int num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == num || board[i][col] == num) {
-                return false;
+            int[][] dp = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                Arrays.fill(dp[i], Integer.MAX_VALUE);
             }
-        }
 
-        int startRow = row - row % 3;
-        int startCol = col - col % 3;
-        for (int i = startRow; i < startRow + 3; i++) {
-            for (int j = startCol; j < startCol + 3; j++) {
-                if (board[i][j] == num) {
-                    return false;
+            PriorityQueue<Node> pq = new PriorityQueue<>();
+            pq.offer(new Node(0, 0, cave[0][0]));
+            dp[0][0] = cave[0][0];
+
+            int[] dx = {1, -1, 0, 0};
+            int[] dy = {0, 0, 1, -1};
+
+            while (!pq.isEmpty()) {//다익스트라
+                Node current = pq.poll();
+
+                for (int i = 0; i < 4; i++) {
+                    int nx = current.x + dx[i];
+                    int ny = current.y + dy[i];
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n) {//범위 내에서
+                        int newCost = current.cost + cave[nx][ny];//계산한 값이
+                        if (dp[nx][ny] > newCost) {//더 작다면 갱신
+                            dp[nx][ny] = newCost;
+                            pq.offer(new Node(nx, ny, newCost));//이어서 진행
+                        }
+                    }
                 }
             }
-        }
 
-        return true;
-    }
-
-    public static String boardToString(int[][] board) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                sb.append(board[i][j]).append(" ");
-            }
-            sb.append("\n");
+            System.out.println("Problem " + tc + ": " + dp[n - 1][n - 1]);
+            tc++;
         }
-        return sb.toString();
     }
 }
