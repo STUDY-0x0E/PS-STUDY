559f06c621ebac80daf3a6f2421c7ff2306ca59c
Hady560 <86261475+Hady560@users.noreply.github.com>
2023-10-17 19:35:12 +0900

    Update 오주현.java

--- 9주차 (23. 10. 10. ~ 23. 10. 15.)/BOJ_10159_저울/오주현.java
+++ 9주차 (23. 10. 10. ~ 23. 10. 15.)/BOJ_10159_저울/오주현.java
@@ -1,33 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] weight = new int[N];
-		int[] value = new int[N];
-		
-		for (int i = 0; i < N; i++) {
+		int link[][] = new int[N+1][N+1]; 
+		StringTokenizer st;
+		for (int i = 1; i <= M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			weight[i]= Integer.parseInt(st.nextToken());
-			value[i]= Integer.parseInt(st.nextToken());
+			int heavy = Integer.parseInt(st.nextToken());
+			int light = Integer.parseInt(st.nextToken());
+			
+			link[heavy][light] = 1;
+			link[light][heavy] = -1;
 		}
 		
-		int[] dp = new int[K+1];
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = K; j >= weight[i]; j--) {
-				dp[j]= Math.max(dp[j], dp[j-weight[i]]+value[i]); 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				for (int k = 1; k <= N; k++) {
+					if (link[j][i] == 1 && link[i][k] == 1) link[j][k] = 1;
+					else if (link[j][i] == -1 && link[i][k] == -1) link[j][k] = -1;
+				}
 			}
 		}
-		System.out.println(dp[K]);
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			int answer = N-1;
+			for (int j = 1; j <= N; j++) {
+				if (link[i][j] != 0) answer--;
+			}
+			sb.append(answer+ "\n");
+		}
+		
+		System.out.println(sb);
 	}
 }
