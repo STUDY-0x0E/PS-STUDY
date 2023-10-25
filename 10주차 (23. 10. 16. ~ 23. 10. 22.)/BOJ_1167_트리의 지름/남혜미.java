ecdb94315ee2e54c964c36f9fd40fce38135dd1d
namhyemi <85321894+namhyemi@users.noreply.github.com>
2023-10-25 09:12:26 +0900

    refact: BOJ1167_남혜미- 문제풀이코드수정

--- 10주차 (23. 10. 16. ~ 23. 10. 22.)/BOJ_1167_트리의 지름/남혜미.java
+++ 10주차 (23. 10. 16. ~ 23. 10. 22.)/BOJ_1167_트리의 지름/남혜미.java
@@ -2,62 +2,83 @@ import java.util.*;
 import java.io.*;
 
 /*
-1. summary 
-	총 F층의 건물에서 S층에서 G층으로 이동하려고 할 때, 최소 버튼 수 구하기
-	이동할 수 없을 경우 "use the stairs" 출력
-2. strategy : BFS 
+1. summary : 트리의 지름
+	트리의 임의의 두 점 사이의 거리 중 가장 긴 것을 선택
+	= 가장 거리가 먼 (가중치의 합이 큰) 두 개의 노드를 선택 
+2. strategy : DFS
 3. note
-	1 ≤ S, G ≤ F ≤ 1,000,000, 0 ≤ U, D ≤ 1,000,000
+	2 ≤ V (정점의 개수) ≤ 100,000
+	주어진 거리는 모두 10000이하의 자연수
 */
 
-public class BOJ5014 {
+public class BOJ1167 {
 
-	static int visited[];
-	static Queue<Integer> floor = new ArrayDeque<>(); 
+	static class Node {
+		int vertex;
+		int weight;
+		
+		public Node(int vertex, int weight) {
+			this.vertex = vertex;
+			this.weight = weight;
+		}
+	}
+	
+	static int N;
+	static List<Node> nodes[];
+	static boolean visited[];
+	
+	static int maxNode = 0, maxWeight = 0;
 	
 	public static void main(String[] args) throws Exception {
-
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 		
-		int F = Integer.parseInt(st.nextToken()); // 건물층 
-		int S = Integer.parseInt(st.nextToken()); // 출발층
-		int G = Integer.parseInt(st.nextToken()); // 목적층
-		int U = Integer.parseInt(st.nextToken()); // 올라갈 때 이동 수
-		int D = Integer.parseInt(st.nextToken()); // 내려갈 때 이동 수
-
-		visited = new int[F+1];
-
-		floor.offer(S);
-		visited[S] = 1;
-		
-		int result = 0;
-		while(!floor.isEmpty()) {
-			int curFloor = floor.poll(); 
-            
-			if(curFloor == G) { // 목적층 도착
-				result = visited[curFloor];
-				break;
-			}
-			
-			int upFloor = curFloor + U; 
-			if(upFloor <= F) { // 건물 층 범위
-				if(visited[upFloor] == 0) { // 미방문 층
-					floor.offer(upFloor);
-					visited[upFloor] = visited[curFloor] + 1;		
-				}
-			}
-			
-			int downFloor = curFloor - D;
-			if(downFloor > 0) { // 건물 층 범위
-				if(visited[downFloor] == 0) { // 미방문
-					floor.offer(downFloor);
-					visited[downFloor] = visited[curFloor] + 1;	
-				}
-			}		
+		// 입력 데이터 저장하기
+		N = Integer.parseInt(br.readLine());
+		nodes = new ArrayList[N+1];
+		for(int i = 0; i < N+1; i++) {
+			nodes[i] = new ArrayList<>();
 		}
 		
-		if(result == 0) System.out.println("use the stairs");
-		else System.out.println(result - 1);
+		for(int i = 1; i < N+1; i++) { // 각 정점마다 연결된 정점 확인
+			st = new StringTokenizer(br.readLine());
+			
+			int k = Integer.parseInt(st.nextToken()); // 하나의 정점
+			
+			int n = Integer.parseInt(st.nextToken()); // 연결된 정점
+			while(n != -1) { // -1 값이 들어오면 더이상 연결된 정점이 없음을 의미
+				int weight = Integer.parseInt(st.nextToken()); // 가중치
+				
+				nodes[k].add(new Node(n, weight));
+				
+				n = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		visited = new boolean[N+1];
+        visited[1] = true;
+		DFS(1, 0);
+		
+		visited = new boolean[N+1];
+        visited[maxNode] = true;
+		DFS(maxNode, 0);
+		
+		System.out.println(maxWeight);
+	}
+
+	public static void DFS(int node, int weight) {
+		
+		if(maxWeight < weight) {
+			maxWeight = weight;
+			maxNode = node;
+		}
+		
+		for(Node n : nodes[node]) {
+			if(visited[n.vertex]) continue; // 이미 방문
+			
+			visited[n.vertex] = true;
+			DFS(n.vertex, weight + n.weight);
+		}
 	}
 }
